eb33358f2ed8047ce65a4f6bb85f7e54b5c1b770
birhanu-tech <birhanualemu99@gmail.com>
2024-10-16 16:24:38 +0900

    updated hamburger menu

--- amazon-part-1/src/componenet/header/Lowerheader.jsx
+++ amazon-part-1/src/componenet/header/Lowerheader.jsx
@@ -1,23 +1,56 @@
-import React from 'react'
+// import React from 'react'
+// import { IoMdMenu } from "react-icons/io";
+// import classes from "./header.module.css";
+
+// function Lowerheader() {
+//   return (
+//     <div className={classes.lower_container}>
+//       <ul>
+//         <li>
+//           <IoMdMenu />
+//           <p>All</p>
+//         </li>
+//         <li>Today's deal</li>
+//         <li>costumer service</li>
+//         <li>Registry</li>
+//         <li>Gift card</li>
+//         <li>sell</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Lowerheader
+
+import React, { useState } from "react";
 import { IoMdMenu } from "react-icons/io";
 import classes from "./header.module.css";
 
 function Lowerheader() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <div className={classes.lower_container}>
-      <ul>
-        <li>
-          <IoMdMenu />
-          <p>All</p>
-        </li>
+      {/* Hamburger icon */}
+      <div className={classes.hamburger} onClick={toggleMenu}>
+        <IoMdMenu size={30} />
+        <p>ALL</p>
+      </div>
+      {/* Collapsible menu */}
+      <ul className={`${classes.menu} ${isMenuOpen ? classes.show : ""}`}>
         <li>Today's deal</li>
-        <li>costumer service</li>
+        <li>Customer service</li>
         <li>Registry</li>
         <li>Gift card</li>
-        <li>sell</li>
+        <li>Sell</li>
       </ul>
     </div>
   );
 }
 
-export default Lowerheader
+export default Lowerheader;
+
